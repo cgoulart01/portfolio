974462a7e5211aa9aa282f7b93c740f91d5df6f7
Caio Vitor Goulart <cgoulart.rj@gmail.com>
2023-06-30 04:09:57 -0300

    feature: intial components portfolio

---
+++ src/components/Portfolio/Portfolio.tsx
@@ -0,0 +1,11 @@
+interface PortfolioProps {}
+
+const Portfolio = ({}: PortfolioProps) => {
+  return (
+    <div>
+      <h1 className="">Portfolio</h1>
+    </div>
+  );
+};
+
+export default Portfolio;
